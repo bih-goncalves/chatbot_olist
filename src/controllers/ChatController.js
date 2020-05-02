f31cfa822a7d96c84355004df571497433d561b0
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-02 06:16:25 -0300

    implement routes for chat flow

--- src/controllers/ChatController.js
+++ src/controllers/ChatController.js
@@ -6,19 +6,61 @@ module.exports = {
 
     listChatForId(request, response, db){
         const {id} = request.params;
-        db.collection('data').save({data_test:"Apenas um teste"}, (err, result) => {
-            if (err) return console.log(err)
-            console.log('Salvo no Banco de Dados')
+        db.collection('chat').find({chatId: id},{_id:0}).toArray((error, documents)=> {
+            if(error) {throw error}
+            response.json({createdAt:documents[0]["created_at"] ,clientName: documents[0]["clientName"],messages: documents[0]["messages"]});
         });
-        db.collection('data').save({data_test:"Apenas mais um teste"}, (err, result) => {
-            if (err) return console.log(err)
-            console.log('Salvo no Banco de Dados')
-        });
-        response.json({data:{listForId: id}});
     },
 
-    saveMessage(request, response){
+    createChat(request, response, db){
+       const {client="Fulano"} = request.body;
+       const id = `${client}-${Date.now()}`
+       const chat = {
+            created_at: new Date(),
+            chatId: id,
+            clientName: client,
+            messages:[]
+       }
+       db.collection('chat').save(chat, (err, result) => {
+        if (err) return console.log(err)
+            console.log('Chat salvo no banco de dados!')
+        });
+        response.json({id:id});
+    },
+
+    saveMessage(request, response, db){
         const {id} = request.params;
-        response.json({data:{messageRegisteredForId:id}})
+        console.log(id)
+        const {message} = request.body;
+        const message_id = `${id}/${Date.now()}`;
+        db.collection('chat').find({chatId:id}).toArray((error, documents) => {
+            if(error){throw error}
+            let newMessages = []
+            if(documents[0]["messages"].length){
+                const content = JSON.parse(documents[0]["messages"]);
+                newMessages = [...content,{
+                    id: message_id,
+                    from: "client",
+                    date: Date(),
+                    text: message
+                }]
+            }else{
+                newMessages = [{
+                    id: message_id,
+                    from: "client",
+                    date: Date(),
+                    text: message
+                }]
+            }
+            
+            db.collection('chat').update({chatId:id},{
+            $set:{
+                created_at: new Date(),
+                messages: JSON.stringify(newMessages)
+            }});
+            // const updatedObjc = {...document, messages: newMessages};
+            // console.log(document);
+            response.json({data:"Mensagem Enviada"});
+        });
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -7,19 +7,23 @@ const botController = require('./controllers/BotController');
 
 
 // CONEXÃO COM O BANCO DE DADOS
-var db = null;
+let db = null;
 var MongoClient = require('mongodb').MongoClient;
 const MONGO_URL = "mongodb://localhost:27017/mydb";
 MongoClient.connect(MONGO_URL, function(err, client) {
     if (err) throw err;
     db = client.db('mydb');
+    db.collection('chat').createIndex({created_at:1},{expireAfterSeconds:600});
     console.log("Database created!");
   });
+
 
 // Rotas para o chat
 routes.get('/chat', chatController.teste);
 routes.get('/chat/:id', (request, response) => chatController.listChatForId(request, response, db));
+routes.post('/chat', (request, response) => chatController.createChat(request, response, db));
 routes.post('/chat/:id', (request, response) => chatController.saveMessage(request, response, db));
+
 
 // Rotas para o mock dos produtos
 routes.get('/products', (request, response) => productController.listProducts(request, response, db));
