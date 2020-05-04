fc33afbf8651831ad53815a2e68a240f18394395
Bianca Gonçalves <bih_goncalves@hotmail.com>
2020-05-04 16:46:04 -0300

    feat: handle olist notification

--- src/routes.js
+++ src/routes.js
@@ -6,26 +6,49 @@ const productController = require('./controllers/ProductController');
 const botController = require('./controllers/BotController');
 const sellerController = require('./controllers/SellerController');
 const mLController = require('./controllers/MLController');
+const mlApi = require('./services/MLAPI');
 
 // Rotas para o chat
 routes.get('/chat', chatController.teste);
-routes.get('/chat/:id', (request, response) => chatController.listChatForId(request, response));
-routes.post('/chat', (request, response) => chatController.createChat(request, response));
-routes.post('/chat/:id', (request, response) => chatController.saveMessage(request, response));
+routes.get('/chat/:id', (request, response) =>
+  chatController.listChatForId(request, response),
+);
+routes.post('/chat', (request, response) =>
+  chatController.createChat(request, response),
+);
+routes.post('/chat/:id', (request, response) =>
+  chatController.saveMessage(request, response),
+);
 
 // Rotas para o mock dos produtos
-routes.get('/products', (request, response) => productController.listProducts(request, response));
-routes.get('/product/:id', (request, response) => productController.getProduct(request, response));
-routes.post('/product', (request, response) => productController.insertProduct(request, response));
+routes.get('/products', (request, response) =>
+  productController.listProducts(request, response),
+);
+routes.get('/product/:id', (request, response) =>
+  productController.getProduct(request, response),
+);
+routes.post('/product', (request, response) =>
+  productController.insertProduct(request, response),
+);
 
 // Rotas para a resposta do bot
 routes.get('/bot/answer', botController.getAnswerForQuestion);
 
 // Rota para responder o cliente após o vendedor ter respondido uma pergunta que antes era desconhecida
-routes.post('/seller/newquestion', (request, response) => sellerController.sendNewQuestionToSeller(request, response));
-routes.post('/seller/newanswer', (request, response) => sellerController.sendNewAnswerToClient(request, response));
+routes.post('/seller/newquestion', (request, response) =>
+  sellerController.sendNewQuestionToSeller(request, response),
+);
+routes.post('/seller/newanswer', (request, response) =>
+  sellerController.sendNewAnswerToClient(request, response),
+);
 
 // Rota de notificação da API do mercado livre
-routes.post('/ml/notifications', (request, response) => mLController.handleMLNotification(request, response));
-module.exports = routes;
+routes.post('/ml/notifications', (request, response) =>
+  mLController.handleMLNotification(request, response),
+);
 
+routes.post('/olist/notifications', (request, response) =>
+  mlApi.handleNotificationFromOlist(request, response),
+);
+
+module.exports = routes;
--- src/services/MLAPI.js
+++ src/services/MLAPI.js
@@ -1,28 +1,56 @@
 const axios = require('axios');
 
-const ML_URLBASE = "https://api.mercadolibre.com"
+const ML_URLBASE = 'https://api.mercadolibre.com';
 
 const ACCESS_TOKEN = process.env.ML_ACCESS_TOKEN;
 
-async function getQuestionFromML(resourceURL){
-    const url = ML_URLBASE + resourceURL;
-    const response = await axios.get(url);
-    const {id,text} = response.data;
-    return {id,text};
+async function getQuestionFromML(resourceURL) {
+  const url = ML_URLBASE + resourceURL;
+  const response = await axios.get(url);
+  const { id, text } = response.data;
+  return { id, text };
 }
 
-async function sendAnswerForQuestion(answer, questionId){
-    const url = `${ML_URLBASE}/answers?access_token=${ACCESS_TOKEN}`;
-    axios.post(url, {
-        "question_id": questionId,
-        "text": answer
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+async function sendAnswerForQuestion(answer, questionId) {
+  const url = `${ML_URLBASE}/answers?access_token=${ACCESS_TOKEN}`;
+  axios
+    .post(url, {
+      question_id: questionId,
+      text: answer,
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+function notifyOlistAPI(text, id) {
+  // ENVIA A PERGUNTA E A ID DELA PRO BACK DA OLIST
+  // QUANDO O VENDEDOR RESPONDER, A API DA OLIST VAI ENVIAR UMA NOTIFICAÇÃO DE VOLTA ATRAVÉS DA ROTA
+  // >> /olist/notifications
+  // ESSA PARTE É MOCKADA
+  // MANDAMOS OS SEGUINTES DADOS PARA A OLIST:
+  //  pergunta, id da pergunta e demais informações que a Olist precise para identificar o produto
+  // ELA, POR SUA VEZ, AO NOTIFICAR DE VOLTA, DEVE RETORNAR ESSES DADOS
+  // JUNTO COM A RESPOSTA DO VENDEDOR
+  console.log('Pergunta enviada para o vendedor da Olist...');
+}
+
+function handleNotificationFromOlist(request, response) {
+  const { text, id, answer } = request.body;
+  const url = `${ML_URLBASE}/answers?access_token=${ACCESS_TOKEN}`;
+  axios
+    .post(url, {
+      question_id: id,
+      text: answer,
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
 
 module.exports = {
-    getQuestionFromML,
-    sendAnswerForQuestion
-}
+  getQuestionFromML,
+  sendAnswerForQuestion,
+  notifyOlistAPI,
+  handleNotificationFromOlist,
+};
