b5c1bfa10a5a0d8a98f1d236e7125e5e38a26e7e
Gaspar <gabriel.sgaspar@yahoo.com.br>
2020-05-04 08:13:02 -0300

    add axios dependency, create communication routes for ML API

--- src/controllers/MLController.js
+++ src/controllers/MLController.js
@@ -1,6 +1,19 @@
+const Ml_api = require('../services/MLAPI');
+
 module.exports = {
-    handleMLNotification(request, response){
-        console.log("CHEGOU NOTIFICAÇÃO DO MERCADO LIVRE!");
+
+    async handleMLNotification(request, response){
         console.log(request.body);
+        const {resource,user_id,topic,application_id} = request.body;
+        if(topic == 'questions'){
+            console.log("CHEGOU NOTIFICAÇÃO DE PERGUNTA NO MERCADO LIVRE!");
+            console.log("Respondendo...")
+            const {id,question} = await Ml_api.getQuestionFromML(resource);
+            //----------------------------------------
+            // Envia a pergunta para o BOT
+            //----------------------------------------
+            Ml_api.sendAnswerForQuestion("Olá, assim que possível lhe responderei. Obrigado.",id);
+        }
+        response.status(200).send();
     }
 }
